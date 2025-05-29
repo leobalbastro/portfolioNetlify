077104af4e23253746f0ab4e6a4f28edd4428b00
Leonardo Balbastro <leobalbastro.arg@gmail.com>
2025-05-29 16:18:41 -0300

    FIX: miniatura al compartir la url por redes sociales

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,7 +7,25 @@ const inter = Inter({ subsets: ["latin"] })
 
 export const metadata: Metadata = {
   title: "Leonardo Balbastro Portfolio",
-  description: "Portfolio de desarrollador Java especializado en Spring Boot y Oracle Database"
+  description: "Portfolio de desarrollador Java especializado en Spring Boot y Oracle Database",
+  openGraph: {
+    title: "Leonardo Balbastro Portfolio",
+    description: "Portfolio de desarrollador Java especializado en Spring Boot y Oracle Database",
+    url: "https://leobalbastro.netlify.app",
+    siteName: "Leonardo Balbastro Portfolio",
+    images: [
+      {
+        url: "Portolio.png",
+        secureUrl: "https://leobalbastro.netlify.app/Portolio.png",
+        type: "image/png",
+        width: 1200,
+        height: 630,
+        alt: "Leonardo Balbastro Portfolio",
+      },
+    ],
+    locale: "es_ES",
+    type: "website",
+  },
 }
 
 export default function RootLayout({
